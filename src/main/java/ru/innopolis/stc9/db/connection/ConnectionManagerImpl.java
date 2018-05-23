e59664cf81114f9d277ba396aa423ec2667956d8
d.ivantsov.stc <d.ivantsov.stc@innopolis.ru>
2018-05-23 18:45:55 +0600

    Created database connection

---
+++ src/main/java/ru/innopolis/stc9/db/connection/ConnectionManagerImpl.java
@@ -0,0 +1,39 @@
+package ru.innopolis.stc9.db.connection;
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManagerImpl implements ConnectionManager {
+
+    private static ConnectionManager connectionManager;
+    static final Logger logger = Logger.getLogger("defaultLog");
+
+    public static ConnectionManager getInstance(){
+        if (connectionManager == null){
+            connectionManager = new ConnectionManagerImpl();
+        }
+        return connectionManager;
+    }
+
+    private ConnectionManagerImpl(){
+
+    }
+
+    @Override
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7239469",
+                    "sql7239469",
+                    "vrKWZga2bt");
+        } catch (ClassNotFoundException | SQLException e) {
+           logger.error(e.getMessage());
+        }
+        return connection;
+    }
+}
---
+++ src/test/java/dbTests/ConnectionTest.java
@@ -0,0 +1,45 @@
+package dbTests;
+
+import com.mysql.jdbc.ConnectionImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.innopolis.stc9.db.connection.ConnectionManager;
+import ru.innopolis.stc9.db.connection.ConnectionManagerImpl;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+
+public class ConnectionTest {
+    ConnectionManager connectionManager = ConnectionManagerImpl.getInstance();
+    Connection connection;
+
+    @Before
+    public void before() {
+        connection = connectionManager.getConnection();
+    }
+
+    @Test
+    public void testConnection() {
+        try (Statement statement = connection.createStatement()) {
+            assertTrue(statement.execute("SHOW TABLES"));
+            statement.execute("SHOW TABLES");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void after() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
