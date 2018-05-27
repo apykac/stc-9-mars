60e7858cd80b2fb37866f5cc76864c7298cad816
alochi <ochirovaldar@gmail.com>
2018-05-28 01:09:48 +0800

    commit MR-6 +spring (login, subject)

--- src/main/java/ru/innopolis/stc9/service/SubjectServiceImpl.java
+++ src/main/java/ru/innopolis/stc9/service/SubjectServiceImpl.java
@@ -1,12 +1,16 @@
 package ru.innopolis.stc9.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.innopolis.stc9.dao.SubjectDao;
 import ru.innopolis.stc9.dao.SubjectDaoImpl;
 import ru.innopolis.stc9.pojo.Subject;
 
 import java.util.List;
 
+@Service
 public class SubjectServiceImpl implements SubjectService {
+    @Autowired
     private SubjectDao subjectDao = new SubjectDaoImpl();
 
     @Override
