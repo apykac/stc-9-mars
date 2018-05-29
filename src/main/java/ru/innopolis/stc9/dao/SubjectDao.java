e016a599f869198d0fc84a4614479fc669d2f915
alochi <ochirovaldar@gmail.com>
2018-05-29 18:09:35 +0800

    feature-MR-6-2

--- src/main/java/ru/innopolis/stc9/dao/SubjectDao.java
+++ src/main/java/ru/innopolis/stc9/dao/SubjectDao.java
@@ -5,5 +5,8 @@ import ru.innopolis.stc9.pojo.Subject;
 import java.util.List;
 
 public interface SubjectDao {
+    boolean addSubject(Subject subject);
+
+    boolean deleteSubject(int subjectId);
     List<Subject> findAllSubject();
 }
