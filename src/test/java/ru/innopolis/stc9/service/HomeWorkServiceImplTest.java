2430c368fa7d82b65943ad2502c7d036c986a4dc
apykac <apykac@mail.ru>
2018-06-15 22:38:33 +0800

    100% покрытие тестами HomeWorkServiceImpl

--- src/test/java/ru/innopolis/stc9/service/HomeWorkServiceImplTest.java
+++ src/test/java/ru/innopolis/stc9/service/HomeWorkServiceImplTest.java
@@ -1,19 +1,25 @@
 package ru.innopolis.stc9.service;
 
+import org.apache.commons.validator.UrlValidator;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import ru.innopolis.stc9.dao.implementation.HomeWorkDaoImpl;
 import ru.innopolis.stc9.dao.interfaces.HomeWorkDao;
+import ru.innopolis.stc9.dao.interfaces.MarkDao;
 import ru.innopolis.stc9.pojo.HomeWork;
+import ru.innopolis.stc9.pojo.Mark;
 import ru.innopolis.stc9.service.implementation.HomeWorkServiceImpl;
 import ru.innopolis.stc9.service.interfaces.HomeWorkService;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -29,57 +35,95 @@ public class HomeWorkServiceImplTest {
     private HomeWorkDao homeWorkDao;
     private HomeWorkService homeWorkService;
     private List<HomeWork> list;
+    private UrlValidator urlValidator;
+    private MarkDao markDao;
 
     @Before
     public void setUp() throws Exception {
-        homeWork = new HomeWork(1,"test", 1, 1);
+        homeWork = new HomeWork(1, "test", 1, 1);
         homeWorkService = new HomeWorkServiceImpl();
         homeWorkDao = PowerMockito.mock(HomeWorkDaoImpl.class);
-        Field field = PowerMockito.field(HomeWorkServiceImpl.class, "homeWorkDao");
-        field.set(homeWorkService, homeWorkDao);
+        markDao = PowerMockito.mock(MarkDao.class);
+        urlValidator = PowerMockito.mock(UrlValidator.class);
+        Field fieldHomeWorkDao = PowerMockito.field(HomeWorkServiceImpl.class, "homeWorkDao");
+        Field fieldMarkDao = PowerMockito.field(HomeWorkServiceImpl.class, "markDao");
+        Field fieldUrlValidator = PowerMockito.field(HomeWorkServiceImpl.class, "urlValidator");
+        fieldHomeWorkDao.set(homeWorkService, homeWorkDao);
+        fieldMarkDao.set(homeWorkService, markDao);
+        fieldUrlValidator.set(homeWorkService, urlValidator);
         list = new ArrayList<>();
     }
 
     @Test
-    public void addHomeWork() throws Exception {
+    public void homeWorkIsURL() {
+        PowerMockito.when(urlValidator.isValid(Mockito.anyString())).thenReturn(true);
+        Assert.assertTrue(homeWorkService.homeWorkIsURL("ValidUrl"));
+    }
+
+    @Test
+    public void addHomeWorkSuccess() {
         PowerMockito.when(homeWorkDao.addHomeWork(homeWork)).thenReturn(true);
+        PowerMockito.when(markDao.addMark(Mockito.anyObject())).thenReturn(true);
         assertTrue(homeWorkService.addHomeWork(homeWork));
     }
 
     @Test
-    public void updateHomeWork() throws Exception {
+    public void addHomeWorkFailed() {
+        PowerMockito.when(homeWorkDao.addHomeWork(homeWork)).thenReturn(false);
+        PowerMockito.when(markDao.addMark(Mockito.anyObject())).thenReturn(false);
+        Assert.assertFalse(homeWorkService.addHomeWork(homeWork));
+    }
+
+    @Test
+    public void updateHomeWork() {
         PowerMockito.when(homeWorkService.updateHomeWork(homeWork)).thenReturn(true);
         assertTrue(homeWorkService.updateHomeWork(homeWork));
     }
 
     @Test
-    public void findById() throws Exception {
+    public void findById() {
         PowerMockito.when(homeWorkDao.findById(1)).thenReturn(homeWork);
         assertEquals(homeWorkService.findById(1), homeWork);
     }
 
     @Test
-    public void findByStudentId() throws Exception {
+    public void findByStudentId() {
         PowerMockito.when(homeWorkDao.findByStudentId(1)).thenReturn(homeWork);
         assertEquals(homeWorkService.findByStudentId(1), homeWork);
     }
 
     @Test
-    public void findByLessonId() throws Exception {
+    public void findByLessonId() {
         PowerMockito.when(homeWorkDao.findByLessonId(1)).thenReturn(homeWork);
         assertEquals(homeWorkService.findByLessonId(1), homeWork);
     }
 
     @Test
-    public void findAllHomeWork() throws Exception {
+    public void findAllHomeWork() {
         PowerMockito.when(homeWorkDao.findAllHomeWork()).thenReturn(list);
         assertEquals(homeWorkService.findAllHomeWork(), list);
     }
 
     @Test
-    public void deleteHomeWork() throws Exception {
+    public void deleteHomeWork() {
         PowerMockito.when(homeWorkDao.deleteHomeWork(1)).thenReturn(true);
         assertTrue(homeWorkService.deleteHomeWork(1));
+    }
+
+    @Test
+    public void getHomeWorkListByLessonId() {
+        List<HomeWork> list = new ArrayList<>(Collections.singletonList(new HomeWork()));
+        PowerMockito.when(homeWorkDao.getHomeWorkListByLessonId(1)).thenReturn(list);
+        Assert.assertEquals(list, homeWorkService.getHomeWorkListByLessonId(1));
+    }
+
+    @Test
+    public void findHomeWorkByMarkId() {
+        Mark mark = new Mark(1, 40, 1, 1, "some comment");
+        PowerMockito.when(markDao.getMarkById(1)).thenReturn(mark);
+        PowerMockito.when(homeWorkDao.findHomeWorkByStudentIdAndLessonId(1, 1)).thenReturn(
+                new HomeWork(1, "url", 1, 1));
+        Assert.assertEquals("url", homeWorkService.findHomeWorkByMarkId(1));
     }
 
 }
