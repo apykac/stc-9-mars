931deb151c9e7b4ad1cb63df4782446133e8fc78
apykac <apykac@mail.ru>
2018-06-28 00:45:13 +0800

    Покрыл тестами UserController

--- src/main/java/ru/innopolis/stc9/controllers/UsersController.java
+++ src/main/java/ru/innopolis/stc9/controllers/UsersController.java
@@ -14,22 +14,22 @@ import java.util.List;
 @Controller
 public class UsersController {
     private UserService userService;
+    private LoginController loginController = new LoginController();
 
     @Autowired
     public UsersController(UserService userService) {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "/admin/edit_user/{id}/delete", method = RequestMethod.POST)
-    public String delUserPost(@PathVariable("id") int id, HttpSession session, Model model) {
-        if (userService.delUserById(id)) return getUserListGet(model);
-        else return editUserGet(id, session, false, model);
+    @RequestMapping(value = "/temp/edit_user", method = RequestMethod.GET)
+    public String editUserGet() {
+        return "redirect:/start";
     }
 
     @RequestMapping(value = "/admin/users_list", method = RequestMethod.GET)
     public String getUserListGet(Model model) {
         model.addAttribute("usersList", userService.getUserList());
-        return "/views/allUsers";
+        return "views/allUsers";
     }
 
     @RequestMapping(value = "/admin/edit_user/{id}", method = RequestMethod.GET)
@@ -46,30 +46,37 @@ public class UsersController {
             model.addAttribute("user", user);
             model.addAttribute("isOwner", isOwner);
         }
-        return "/views/editUser";
+        return "views/editUser";
     }
 
-    @RequestMapping(value = "/temp/edit_user", method = RequestMethod.GET)
-    public String editUserGet() {
-        return "redirect:/start";
+    @RequestMapping(value = "/admin/edit_user/{id}/delete", method = RequestMethod.POST)
+    public String delUserPost(@PathVariable("id") int id,
+                              HttpSession session,
+                              Model model) {
+        if (userService.delUserById(id)) return getUserListGet(model);
+        else return editUserGet(id, session, false, model);
     }
 
     @RequestMapping(value = "/temp/edit_user", method = RequestMethod.POST)
-    public String editUserPost(@RequestBody MultiValueMap<String, String> incParam, HttpSession session, Model model) {
+    public String editUserPost(@RequestBody MultiValueMap<String, String> incParam,
+                               HttpSession session,
+                               Model model) {
         Object[] info = userService.editUser(incParam);
-        if (((List) info[0]).isEmpty()) info[0] = null;
-        if (((List) info[1]).isEmpty()) info[1] = null;
+        if (((List) info[0]).isEmpty())
+            info[0] = null;
+        if (((List) info[1]).isEmpty())
+            info[1] = null;
         if ((Boolean) info[2]) updateSession(session);
         model.addAttribute("errors", info[0]);
         model.addAttribute("success_list", info[1]);
-        return editUserGet(Integer.parseInt(incParam.get("id").get(0)),
-                session,
-                Boolean.parseBoolean(incParam.get("isOwner").get(0)),
-                model);
+        boolean isOwner = Boolean.parseBoolean(incParam.get("isOwner").get(0));
+        int id = Integer.parseInt(incParam.get("id").get(0));
+        return editUserGet(id, session, isOwner, model);
     }
 
     @RequestMapping(value = "/university/profile", method = RequestMethod.GET)
-    public String editOwnerProfile(HttpSession session, Model model) {
+    public String editOwnerProfile(HttpSession session,
+                                   Model model) {
         return editUserGet((Integer) session.getAttribute(SessionDataInform.ID),
                 session,
                 true,
@@ -78,15 +85,17 @@ public class UsersController {
 
     @RequestMapping(value = "/university/profile/delete", method = RequestMethod.GET)
     public String deleteAccountGet() {
-        return "/views/deleteAccountPage";
+        return "views/deleteAccountPage";
     }
 
     @RequestMapping(value = "/university/profile/delete", method = RequestMethod.POST)
-    public String deleteAccountPost(@RequestParam(value = "password") String candidate, HttpSession session, Model model) {
+    public String deleteAccountPost(@RequestParam(value = "password") String candidate,
+                                    HttpSession session,
+                                    Model model) {
         int id = (Integer) session.getAttribute(SessionDataInform.ID);
         if (userService.checkPasswordOfCurrentAccount(id, candidate))
             if (userService.deactivationCurrentAccount(id))
-                return new LoginController().logout(session);
+                return loginController.logout(session);
             else model.addAttribute("error", "Error deactivation by DAO");
         else model.addAttribute("error", "Incorrect password");
         return deleteAccountGet();
